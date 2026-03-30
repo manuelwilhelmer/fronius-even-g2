fbce484f737a9e57c46d6ba492092d6d24fb6b8b
Manuel Wilhelmer <manuelwilhelmer@Manuel-Wilhelmers-MacBook.local>
2026-03-30 22:18:24 +0200

    fix(build): bundle complete app into a single offline html to bypass hardware CORS

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react(), tailwindcss()],
-  build: {
-    assetsDir: '',
-  },
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   server: {
     host: true,
     port: 5173,
