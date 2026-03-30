ec37cd9bc377632b7027694e1ac21c5e94ac46e7
Manuel Wilhelmer <manuelwilhelmer@Manuel-Wilhelmers-MacBook.local>
2026-03-30 22:05:50 +0200

    fix(build): avoid evenhub-cli asset folder path resolution bug

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: '',
+  },
   server: {
     host: true,
     port: 5173,
